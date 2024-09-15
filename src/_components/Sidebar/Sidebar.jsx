c93c38835804bc3304ad959134d6c2eff6dcb4ea
Skittz <skittz.dev@gmail.com>
2024-09-14 21:20:04 -0300

    feat(components): Sidebar

---
+++ src/_components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from './Sidebar.module.css';
+import bgSidebar from '../../assets/bg-sidebar.jpg';
+import avatarProfile from '../../assets/avatar-profile.jpg';
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img src={bgSidebar} className={styles.cover} alt="Background Profile" />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src={avatarProfile}
+          alt="Avatar Profile"
+        />
+        <strong className="text-base leading-relax">Skittz</strong>
+        <span className="text-sm leading-relax">Developer Front-end</span>
+      </div>
+      <footer>
+        <a className="bold" href="#">
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
