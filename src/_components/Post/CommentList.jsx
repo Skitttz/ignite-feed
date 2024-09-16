33a171f94b1041a1873847ed100e8fc529ebc903
Skittz <skittz.dev@gmail.com>
2024-09-16 06:31:15 -0300

    refact(CommentList): add delete comment feature and map data

--- src/_components/Post/CommentList.jsx
+++ src/_components/Post/CommentList.jsx
@@ -1,12 +1,24 @@
 import { Comment } from './Comment';
 import styles from './CommentList.module.css';
 
-export function CommentList() {
+export function CommentList({ comments, setComments }) {
+  const removeComment = (commentToDelete) => {
+    const commentWithoutDeleted = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+    setComments(commentWithoutDeleted);
+  };
   return (
     <div className={styles.commentList}>
-      <Comment />
-      <Comment />
-      <Comment />
+      {comments.map((comment, index) => {
+        return (
+          <Comment
+            key={index}
+            commentText={comment}
+            removeComment={removeComment}
+          />
+        );
+      })}
     </div>
   );
 }
