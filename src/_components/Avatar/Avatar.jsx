40aae85ace46ca157457818ed18253faea2e8f21
Skittz <skittz.dev@gmail.com>
2024-09-16 06:29:58 -0300

    refact(Avatar): better name class

--- src/_components/Avatar/Avatar.jsx
+++ src/_components/Avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import styles from './Avatar.module.css';
 export function Avatar({ src, hasBorder = true }) {
   return (
     <img
-      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      className={hasBorder ? styles.avatarWithBorder : styles.avatarComment}
       src={src}
       alt="Avatar User"
     />
