f548b23a8063b054fcf24964ad31e18f00a3227b
Skittz <skittz.dev@gmail.com>
2024-09-16 06:35:47 -0300

    refact(CommentForm): create handleNewCommentChange | handleCreateNewComment | handleCommentInvalid

--- src/_components/Post/CommentForm.jsx
+++ src/_components/Post/CommentForm.jsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import styles from './CommentForm.module.css';
 
-export function CommentForm() {
+export function CommentForm({ comments, setComments }) {
+  const [newCommentText, setNewCommentText] = useState('');
+  const handleNewCommentChange = (event) => {
+    setNewCommentText(event.target.value);
+  };
+  const handleCreateNewComment = (event) => {
+    event.preventDefault();
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  };
+
+  const handleCommentInvalid = (event) => {
+    event.target.setCustomValidity('Este campo eh obrigatorio');
+  };
+
   return (
-    <form className={styles.commentForm}>
+    <form className={styles.commentForm} onSubmit={handleCreateNewComment}>
       <strong className="leading-relax">Deixe seu feedback</strong>
-      <textarea placeholder="Deixe um comentario"></textarea>
+      <textarea
+        placeholder="Deixe um comentario"
+        value={newCommentText}
+        onChange={handleNewCommentChange}
+        onInvalid={handleCommentInvalid}
+        required
+      ></textarea>
       <footer>
         <button type="submit">Comentar</button>
       </footer>
