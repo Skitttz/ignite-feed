39ce548f18c032a3cc3915072e9af8271a3f2bc0
Skittz <skittz.dev@gmail.com>
2024-09-16 06:37:41 -0300

    refact(App): use db.json data and iterate values

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/index.css';
 import { Header } from './_components/Header/Header';
 import { Sidebar } from './_components/Sidebar/Sidebar';
 import { Post } from './_components/Post/Post';
+import Posts from './db.json';
 
 function App() {
   return (
@@ -10,7 +11,18 @@ function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <Post />
+        <div>
+          {Posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.descriptions}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
