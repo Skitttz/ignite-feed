04c912b99a9a5ed189719091d721284635fe46c5
SKitttz <skittz.dev@gmail.com>
2024-09-18 14:28:23 -0300

    refact(Sidebar): trigger openModal via button, style changes, and use of exported constants

--- src/_components/Sidebar/Sidebar.tsx
+++ src/_components/Sidebar/Sidebar.tsx
@@ -1,23 +1,42 @@
 import styles from './Sidebar.module.css';
-import bgSidebar from '../../assets/bg-sidebar.jpg';
-import avatarProfile from '../../assets/avatar-profile.jpg';
-import { PencilLine } from 'phosphor-react';
-import { Avatar } from '../Avatar/Avatar';
+import bgSidebar from '@assets/bg-sidebar.jpg';
+import avatarProfile from '@assets/avatar-profile.jpg';
+import { Avatar } from '@components/Avatar/Avatar';
+import Button from '@components/Button/Button';
+import { PencilLine, NotePencil } from 'phosphor-react';
+import { useModal } from '../../hooks/useModal';
+import { PostForm } from '@components/Post/PostForm/PostForm';
+import { Modal } from '@components/Modal/Modal';
+import { profileUser } from '@utils/constantsUser';
 
 export function Sidebar() {
+  const { handleOpenModal, visibleModal, handleCloseModal } = useModal();
   return (
     <aside className={styles.sidebar}>
       <img src={bgSidebar} className={styles.cover} alt="Background Profile" />
       <div className={styles.profile}>
         <Avatar src={avatarProfile} />
-        <strong className="text-base leading-relax">Skittz</strong>
-        <span className="text-sm leading-relax">Mid-Level Developer</span>
+        <strong className="text-base leading-relax">{profileUser.name}</strong>
+        <span className="text-sm leading-relax">{profileUser.role}</span>
       </div>
       <footer>
-        <a className="bold" href="#">
-          <PencilLine size={20} />
-          Editar seu Perfil
-        </a>
+        <Button
+          Icon={PencilLine}
+          text="Editar seu Perfil"
+          typeClass="primary"
+        />
+        <Button
+          Icon={NotePencil}
+          text="Criar Post"
+          typeClass="secondary"
+          onClick={handleOpenModal}
+        />
+        {visibleModal && (
+          <Modal
+            children={<PostForm handleCloseModal={handleCloseModal} />}
+            onClose={handleCloseModal}
+          />
+        )}
       </footer>
     </aside>
   );
