ccb77fd24470592ca705c2c377feed2a476ac2fa
SKitttz <skittz.dev@gmail.com>
2024-09-18 14:26:28 -0300

    feat(utils): add static constants to project

---
+++ src/utils/constantsToast.ts
@@ -0,0 +1,4 @@
+export const toastTimer = {
+  timerEndAnimation:2000,
+  timerStateToast:2500,
+}
---
+++ src/utils/constantsUser.ts
@@ -0,0 +1,7 @@
+import avatarProfile from '@assets/avatar-profile.jpg';
+
+export const profileUser = {
+  name: "Skittz",
+  role:"Mid-Level Developer",
+  avatar:avatarProfile,
+}
